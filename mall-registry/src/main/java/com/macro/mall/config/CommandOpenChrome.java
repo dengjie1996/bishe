d3f3fa1dd1b7e7b3584a50f9d534ba910e39df08
邓杰 <1280075151@qq.com>
2020-04-04 15:28:50 +0800

    项目启动后自动打开浏览器

---
+++ mall-registry/src/main/java/com/macro/mall/config/CommandOpenChrome.java
@@ -0,0 +1,36 @@
+/*
+ * 类名称:CommandOpenChrome.java
+ * 包名称:com.macro.mall.config
+ *
+ * 修改履历:
+ *      日期                修正者        主要内容
+ *
+ *
+ *创建日期：2020/4/4 15:20
+ *create by dengjie
+ * Copyright (c) 2019-2019
+ */
+package com.macro.mall.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 在项目启动后会自动扫描CommandLineRunner 实现类中的run 方法，并执行。
+ **/
+@Component
+public class CommandOpenChrome implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        if (true) {
+            //打开注册中心页面
+            String cmd = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" + " " + "http://localhost:8001";
+            Runtime run = Runtime.getRuntime();
+            try {
+                run.exec(cmd);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
