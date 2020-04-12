f7cafe265ea6cb4b7d81517864bb28c96b384795
邓杰 <1280075151@qq.com>
2020-04-12 16:01:14 +0800

    改了一些配置

---
+++ mall-registry/src/main/java/com/macro/mall/config/EurekaStateChangeListener.java
@@ -0,0 +1,53 @@
+/*
+ * 类名称:EurekaStateChangeListener.java
+ * 包名称:com.macro.mall.config
+ *
+ * 修改履历:
+ *      日期                修正者        主要内容
+ *
+ *
+ *创建日期：2020/4/11 22:56
+ *create by dengjie
+ * Copyright (c) 2019-2019
+ */
+package com.macro.mall.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ */
+@Component
+public class EurekaStateChangeListener {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent eurekaInstanceCanceledEvent) {
+        //服务断线事件
+        logger.warn("服务断线了 =_= :{}", eurekaInstanceCanceledEvent.toString());
+    }
+
+    @EventListener
+    public void listen(EurekaInstanceRegisteredEvent event) {
+        logger.warn(" 有新的服务上线了 >_> : {}", event.toString());
+    }
+
+    @EventListener
+    public void listen(EurekaInstanceRenewedEvent event) {
+        logger.warn(" 服务续约 O_O : {}", event.toString());
+    }
+
+    @EventListener
+    public void listen(EurekaRegistryAvailableEvent event) {
+        logger.warn(" 注册中心启动事件 !_！ : {}", event.toString());
+    }
+
+    @EventListener
+    public void listen(EurekaServerStartedEvent event) {
+        //Server启动
+        logger.warn(" 注册中心Server启动 》》》_》》》！ : {}", event.toString());
+    }
+}
